a2ef986082e4445c631dcec65d66b0c406830c1c
brunannunes <brunanunes324@gmail.com>
2024-03-05 16:35:39 -0300

    outroo

--- src/components/Lista.jsx
+++ src/components/Lista.jsx
@@ -1,10 +1,64 @@
-import { View, StyleSheet, Text } from "react-native"
+import { View, StyleSheet, Text, FlatList } from "react-native"
+import { Usuario } from "./Usuario"
 
 
 export function Lista(){
+
+    const listaUsuarios = [
+        {
+            codigo: '1',
+            nome: 'João',
+            email: 'joao@gmail.com',
+            telefone: '(19) 1234-5678'
+        },
+        {
+            codigo: '2',
+            nome: 'Maria',
+            email: 'maria@gmail.com',
+            telefone: '(19) 9876-5432'
+        },
+        {
+            codigo: '3',
+            nome: 'Rapahel',
+            email: 'raphael@gmail.com',
+            telefone: '(19) 4002-8922'
+        },
+        {
+            codigo: '4',
+            nome: 'Mario',
+            email: 'mario@gmail.com',
+            telefone: '(19) 4002-8922'
+        },
+        {
+            codigo: '5',
+            nome: 'Julio',
+            email: 'julio@gmail.com',
+            telefone: '(19) 4002-8922'
+        },
+        {
+            codigo: '6',
+            nome: 'Caio',
+            email: 'caio@gmail.com',
+            telefone: '(19) 4002-8922'
+        },
+    ]
+
+
     return(
         <View >
-           <Text style={estilos.texto}>Lista</Text>
+          <FlatList
+            data={listaUsuarios}
+            keyExtractor={(item) => {item.codigo}}
+            renderItem={({item}) => (
+                <Usuario
+                nome={item.nome}
+                email={item.email}
+                telefone={item.telefone}
+                />
+                   
+               
+            )}
+          />
         </View>
     )
 }
@@ -14,5 +68,8 @@ const estilos = StyleSheet.create({
     texto:{
       color: '#000',
       marginTop: 20
+    },
+    item:{
+        color:'#000'
     }
 })
---
+++ src/components/Usuario.jsx
@@ -0,0 +1,48 @@
+import { View, StyleSheet, TouchableOpacity, Text } from "react-native";
+
+export function Usuario({nome,email,telefone}){
+    return(
+        <View style={estilos.conteiner}>
+
+            <View style={estilos.conteinerDados}>
+                <Text style={estilos.item}>{nome}</Text>
+                <Text style={estilos.item}>{email}</Text>
+                <Text style={estilos.item}>{telefone}</Text>
+            </View>
+
+            <TouchableOpacity style={estilos.botao}>
+
+                <Text style={estilos.textoBotao}>-</Text>
+            </TouchableOpacity>
+
+        </View>
+    )
+}
+
+const estilos = StyleSheet.create({
+    conteiner:{
+        flexDirection: 'row',
+        borderStartColor:'#fff',
+        margin: 5
+    },
+    conteinerDados:{
+        flex: 1,
+        backgroundColor: '#fff',
+        borderRadius: 10
+    },
+    botao:{
+        justifyContent:'center',
+        alignItems:'center',
+        height:55,
+        width:70,
+        backgroundColor:'#3a5a40',
+        marginStart: 10,
+        borderRadius:5,
+        fontSize: 16
+    },
+    textoBotao:{
+        color: '#fff',
+        fontSize:26
+    },
+
+})
