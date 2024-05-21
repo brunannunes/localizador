11bc0bbee718599a506456da53a6db66ff09f24b
Bruna Nunes Lima da Cruz <38061029880@>
2024-05-21 16:23:44 -0300

    com mapa

--- src/telas/Inicial.tsx
+++ src/telas/Inicial.tsx
@@ -1,24 +1,76 @@
 import {StyleSheet, View, ImageBackground} from 'react-native'
 import {Cabecalho} from '../componentes/Cabecalho'
 import { Botao } from '../componentes/Botao';
-
+import { 
+  requestForegroundPermissionsAsync, 
+  getCurrentPositionAsync, 
+  LocationObject, 
+  watchPositionAsync,
+  LocationAccuracy} from 'expo-location';
+import { useEffect, useState, useRef } from 'react';
+import MapView, { Marker } from 'react-native-maps';
 
 export const Inicial = () => {
+  const [location, setLocation] = useState<LocationObject | null>(null);
+
+
+  const mapRef = useRef<MapView>(null);
+
+async function requestLocationPermission(){
+  const {granted} = await requestForegroundPermissionsAsync();
+
+  if(granted){
+    const currentPosition = await getCurrentPositionAsync();
+    setLocation(currentPosition);
+
+   
+  }
+  
+}
+
+useEffect(()=>{
+  requestLocationPermission();
+
+},[]);
+
+useEffect(() => {
+  watchPositionAsync({
+    accuracy: LocationAccuracy.Highest,
+    timeInterval:1000,
+    distanceInterval:1
+  }, (response) =>{
+    setLocation(response);
+    mapRef.current?.animateCamera({
+      pitch: 70
+    })
+
+  });
+}, []);
+
   return (
     <View style={estilos.conteiner}>
 
-      <Cabecalho titulo="Localizador"/>
-
-        
-      <ImageBackground
-                style={estilos.fundo}
-                source={require('../../assets/mapa.png')}
-            >
-                <Botao texto='Teste 01' />
-                <Botao texto='Teste 02' />
-                <Botao texto='Teste 03' />
-
-            </ImageBackground>
+      {
+          location &&
+            <MapView
+            ref={mapRef}
+            style={estilos.map}
+            initialRegion={{
+              latitude:location.coords.latitude,
+              longitude:location.coords.longitude,
+              latitudeDelta: 0.005,
+              longitudeDelta: 0.005
+        }}
+        >
+          <Marker
+            coordinate={{
+              latitude:location.coords.latitude,
+              longitude:location.coords.longitude,
+            }}
+          />
+          </MapView>
+      }
+     
 
     </View>
   );
@@ -35,5 +87,9 @@ const estilos = StyleSheet.create({
     flexWrap: 'wrap',
     resizeMode: 'cover'
 },
+  map:{
+    flex:1,
+    width:'100%',
+  }
 });
 
